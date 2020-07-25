f4178fc5a59c35b658de4829002fc28081899f05
Varghese Abraham <varnit2010@gmail.com>
2020-07-25 20:32:44 +0530

    jwt token auth impl

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # core-api-lib (Work in progress)
 
 **Core library with all node microservice utilties**
+ - JWT username/password authentication (Token generation & verification)
  - Password encryption/descryption using "bcryptjs"
  - Error handlers
  - logger service
 
  **TODO**
  - API request & response handler
- - JWT authentication (Token generation & verification)
---
+++ lib/middleware/auth.middleware.ts
@@ -0,0 +1,61 @@
+import { Request, Response, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+import * as fs from 'fs';
+
+export interface IAuthConfig {
+    PRIVATE_KEY_PATH: string;
+    PUBLIC_KEY_PATH: string;
+    WHITELISTED_URLS: string[];
+    privateKey?: any;
+    publicKey?: any;
+}
+
+let authConfig: IAuthConfig | undefined = undefined;
+
+export const init = (config: IAuthConfig) => {
+    authConfig = config;
+    // use 'utf8' to get string instead of byte array  (512 bit key)
+    authConfig.privateKey = fs.readFileSync(authConfig.PRIVATE_KEY_PATH, 'utf8');
+    authConfig.publicKey = fs.readFileSync(authConfig.PUBLIC_KEY_PATH, 'utf8')
+}
+
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+    if (!authConfig) {
+        res.status(401).send(`NOT_AUTHORIZED`);
+    }
+    if (!authConfig?.WHITELISTED_URLS.includes(req.url)) {
+        //Try to validate the token and get data
+        try {
+            //Get the jwt token from the head
+            let token = <string>req.headers["authorization"];
+            token = token.split(' ')[1];
+            let jwtPayload;
+            jwtPayload = <any>jwt.verify(token, authConfig?.privateKey);
+            res.locals.jwtPayload = jwtPayload;
+        } catch (error) {
+            //If token is not valid, respond with 401 (unauthorized)
+            res.status(401).send(`NOT_AUTHORIZED`);
+            return;
+        }
+    }
+
+    //Call the next middleware or controller
+    next();
+};
+
+export const generateJwt = (username: string, permissions: any) => {
+    if (!authConfig) {
+        return 'No auth config found';
+    }
+    let jwtPayload = {
+        username: username,
+        permissions: permissions
+    };
+
+    //The token is valid for 1 hour
+    //We want to send a new token on every request
+    const newToken = jwt.sign(jwtPayload, authConfig?.privateKey, {
+        expiresIn: "1h"
+    });
+    return newToken;
+};
